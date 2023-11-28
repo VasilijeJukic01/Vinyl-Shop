55c82d1cb5497b41aa6927aa2c912443a9c08142
Vasilije Jukic <vasilijejukic@gmail.com>
2023-11-28 18:42:20 +0100

    Changed port for app service

--- app_service/app.js
+++ app_service/app.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });
 
-app.listen(8000);
+app.listen(9000);
 
 app.use(express.static(path.join(__dirname, 'static')));
 
